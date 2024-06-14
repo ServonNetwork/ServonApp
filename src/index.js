39bfa0b7babd32f94e18e91cad1e929bb09e2b7e
Blonicx <belte_YT@outlook.com>
2024-06-14 13:56:55 +0200

    Added Logo, Mainteance and Error Page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 const { app, BrowserWindow } = require('electron');
+const axios = require('axios');
+const path = require('path');
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
+    icon: __dirname + '/assets/images/logo.ico',
+
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
+      preload: path.join(__dirname, '/javascript/preload.js')
     },
   });
 
   mainWindow.maximize();
   mainWindow.setMenuBarVisibility(false);
-  mainWindow.loadFile('src/index.html');
+
+  axios.get('http://localhost:3000/state')
+    .then(response => {
+      const state = response.data;
+      if (state.maintenanceMode) {
+        mainWindow.loadFile('src/pages/MaintenanceWork.html');
+      } else {
+        mainWindow.loadFile('src/index.html');
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching state from server:', error);
+      mainWindow.loadFile('src/pages/Error.html');
+    });
 }
 
 app.on('ready', createWindow);
