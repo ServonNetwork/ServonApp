fbdbdad78bc02203a999b3ebf12208ee42ef7750
Blonicx <belte_YT@outlook.com>
2024-06-14 16:11:24 +0200

    Added Quality of Life Features and reworked the Ui

--- src/javascript/frontend/buttons.js
+++ src/javascript/frontend/buttons.js
@@ -1,4 +1,11 @@
+const { app} = require('electron');
+
 function DownloadServon(){
     window.open("https://github.com/Blonicx/Servon-Api/releases/download/V0.6/Servon-Api.jar", "_self")
     console.log("Downloaded Servon-Api")
 };
+
+function Restart(){
+    app.relaunch()
+    app.exit()
+}
